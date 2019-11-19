0732bc5c55e967b345dc25e553c9dc66ad899e26
Бездетный Николай <Nekooolay@mail.ru>
2019-11-19 20:32:55 +0300

    Test

--- MyMailClient/MailBox.cs
+++ MyMailClient/MailBox.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Controls;
-using MailKit;
-using MailKit.Net.Imap;
-using MimeKit;
 
 namespace MyMailClient
 {
